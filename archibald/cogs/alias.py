a1e8e75caae36ab313255bb1a0613fd222d56963
red <red@kalab.sk>
2023-09-20 21:34:51 +0200

    add links json path to config, formatting

--- archibald/cogs/alias.py
+++ archibald/cogs/alias.py
@@ -3,23 +3,26 @@ from discord.ext import commands
 from discord.app_commands import describe
 from discord.ext.commands.context import Context
 
+from archibald.config import LINKS_JSON_PATH
+
+
 class LinkAlias(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.links_fp = "archibald/cogs/links.json"
-        self.link_map: dict[str, str] = json.loads(open(self.links_fp).read())['links'];
+        self.links_fp = LINKS_JSON_PATH
+        self.link_map: dict[str, str] = json.loads(open(self.links_fp).read())["links"]
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: Context, error):
-        await ctx.send(f'{error} {ctx.author.name}')
+        await ctx.send(f"{error} {ctx.author.name}")
 
     @commands.hybrid_command()
     @describe(alias="the alias of the link you want")
     async def link(self, ctx: Context, alias: str):
-        if (alias in self.link_map):
+        if alias in self.link_map:
             await ctx.send(self.link_map[alias])
         else:
-            await ctx.send(f'no link found for alias {alias}', ephemeral=True) 
+            await ctx.send(f"no link found for alias {alias}", ephemeral=True)
 
     @commands.hybrid_command()
     async def showaliases(self, ctx):
@@ -29,19 +32,14 @@ class LinkAlias(commands.Cog):
         await ctx.reply(alias_list)
 
     @commands.hybrid_command()
-    @describe(
-        alias="alias you want to add",
-        link="link to be accessed via the alias"
-    )
-    @commands.has_any_role(
-        872083306668261437, # admin
-        1000862083639943228 # mod
-    )
+    @describe(alias="alias you want to add", link="link to be accessed via the alias")
+    @commands.has_any_role(872083306668261437, 1000862083639943228)  # admin  # mod
     async def addalias(self, ctx: Context, alias: str, link: str):
         self.link_map[alias] = link
         with open(self.links_fp, "w") as f:
             json.dump({"links": self.link_map}, f)
         await ctx.reply(f"added alias {alias} for link {link}", ephemeral=True)
+
 
 async def setup(bot):
     await bot.add_cog(LinkAlias(bot))
--- archibald/config.py
+++ archibald/config.py
@@ -1,3 +1,5 @@
 from os import environ
 
 BOT_TOKEN = environ["BOT_TOKEN"]
+
+LINKS_JSON_PATH = environ.get("LINKS_JSON_PATH", "/mnt/links.json")
