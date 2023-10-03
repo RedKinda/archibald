9d04800a76a2f4f4e7c45244ae5e3a249b54b9ff
red <red@kalab.sk>
2023-10-03 15:31:52 +0200

    add susbot cog

---
+++ archibald/cogs/susbot.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import random
+from typing import TYPE_CHECKING
+from discord import Message
+from discord.ext import commands
+
+if TYPE_CHECKING:
+    from archibald.main import Archibald
+
+
+class Susbot(commands.Cog):
+    def __init__(self, bot: Archibald):
+        self.bot = bot
+        self.phrase_mappings = {
+            "grind": "�",
+            "cum laude": "�",
+            "gpa": "�",
+        }
+
+    @commands.Cog.listener()
+    async def on_message(self, message: Message):
+        for phrase, emoji in self.phrase_mappings.items():
+            if phrase in message.content.lower():
+                if random.random() < 0.2:
+                    await message.add_reaction(emoji)
+
+
+async def setup(bot):
+    await bot.add_cog(Susbot(bot))
