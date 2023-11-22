46426ea8f4a469cc81e4a177da7cda9443be6b42
michaeltout <toutonghi.michael@gmail.com>
2023-11-22 20:06:56 +0100

    Remove unnecessary code

--- archibald/config.py
+++
@@ -1,9 +0,0 @@
-from os import environ, getenv
-from dotenv import load_dotenv
-
-DOTENV_PATH = getenv("DOTENV_PATH", None)
-load_dotenv(DOTENV_PATH)
-
-BOT_TOKEN = environ["BOT_TOKEN"]
-
-LINKS_JSON_PATH = environ.get("LINKS_JSON_PATH", "/mnt/links.json")
